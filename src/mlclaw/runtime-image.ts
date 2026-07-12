93fa15246f5d568b764057d19e2628a726b4ccf8
Onur Solmaz <2453968+osolmaz@users.noreply.github.com>
2026-07-12 16:50:12 +0800

    chore(runtime): pin merged BrokerKit revision

--- src/mlclaw/runtime-image.ts
+++ src/mlclaw/runtime-image.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "node:url";
 
 const DEFAULT_OPENCLAW_VERSION = "2026.7.1-beta.5";
 const DEFAULT_BROKERKIT_PLUGIN_VERSION = "0.1.0";
-const DEFAULT_BROKERKIT_VERSION = "13a0f5ff813d749a1e477ffac1c43c6e47ffdb59";
+const DEFAULT_BROKERKIT_VERSION = "1c5755b4c2e05b94f93ba3c64d04c757b06d7e8c";
 const DEFAULT_RUNTIME_IMAGE_REPOSITORY = "ghcr.io/osolmaz/mlclaw";
 
 const PACKAGE_METADATA = readPackageMetadata();
